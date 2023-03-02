76ee934f96b6d4ceee4a2c8c8297fba747eae819
rin <hello@rin.cn>
2023-03-02 21:00:02 +0800

    hi

---
+++ hello/src/main/java/com/key/hello/controller/controller.java
@@ -0,0 +1,16 @@
+package com.key.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class controller {
+    @GetMapping("")
+    public String hello(){
+        System.out.println("hello");
+        return "hi";
+    }
+
+}
